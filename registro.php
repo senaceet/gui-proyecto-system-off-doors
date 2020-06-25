4369c559acdbc8393ef85338b470c1025fab03cd
afosorio742 <64913352+afosorio742@users.noreply.github.com>
2020-06-25 08:28:07 -0500

    modificaciones

---
+++ registro.php
@@ -0,0 +1,12 @@
+<?php 
+include  ("conexion.php");
+
+$primernombre = (isset($_POST['primernombre']))?$_POST['primernombre']:"";
+$segundonombre = (isset($_POST['segundonombre']))?$_POST['segundonombre']:"";
+$primerapellido = (isset($_POST['primerapellido']))?$_POST['primerapellido']:"";
+$segundonombre = (isset($_POST['segundoapellido']))?$_POST['segundoapellido']:"";
+
+
+
+
+?>
